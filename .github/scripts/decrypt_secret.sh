47f408bd0d89c2af87168e152819f1c4c84f5094
jvsrikanth11 <66364743+jvsrikanth11@users.noreply.github.com>
2021-10-07 14:00:31 -0400

    Update decrypt_secret.sh

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,4 +5,4 @@ cd .github/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASS" \
---output .github/secrets/app_secrets.json app_secrets.json.gpg
+--output app_secrets.json app_secrets.json.gpg
